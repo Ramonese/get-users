5d8412e04f4eac06b047e4fe3cec68685f8799cf
Ieva Ozolite <ieva.ozolite@gmail.com>
2019-06-20 23:45:56 +0300

    search form html

--- users/src/App.vue
+++ users/src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container bg-light">
+       <h1 class="border-bottom h3">Users</h1>
+      <div class="row">
+       
+        <div class="col">
+          <form>
+
+            <div class="form-group">
+              <div class="row">
+                <div class="col col-md-auto">
+                  <label for="search" class="sr-only">search users</label>
+                  <input
+                    type="text"
+                    class="form-control form-control-sm"
+                    id="search"
+                    aria-describedby="search user"
+                    placeholder="Enter phone number"
+                  >
+                </div>
+                <div class="col col-lg-1">
+                  <button type="submit" class="btn btn-primary btn-sm">Search</button>
+                </div>
+            </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    
+    <!-- <Users/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Users from "./components/Users.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Users
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
